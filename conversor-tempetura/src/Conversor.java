e98f6fc053e6ec49fe9501b3ac499af79a643aa3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-17 18:53:14 -0300

    com casting

--- conversor-tempetura/src/Conversor.java
+++ conversor-tempetura/src/Conversor.java
@@ -3,11 +3,18 @@ public class Conversor {
         double temperaturaCelsius;
         double temperaturaFahrenheit;
         String resultado;
+        int temperaturaFahrenheitEmInt;
 
         temperaturaCelsius = 23.40;
         temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
 
         resultado = String.format("%.2fºC(Celsius) para Fahrenheit é %.2fºF", temperaturaCelsius, temperaturaFahrenheit);
+
+        System.out.println(resultado);
+
+        temperaturaFahrenheitEmInt = (int) temperaturaFahrenheit;
+
+        resultado = String.format("%.2fºC(Celsius) para Fahrenheit é %dºF", temperaturaCelsius, temperaturaFahrenheitEmInt);
 
         System.out.println(resultado);
     }
