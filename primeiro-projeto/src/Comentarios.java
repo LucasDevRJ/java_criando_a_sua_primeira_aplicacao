136917f18bd721ca9e55268ea7cde1c1f7865372
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-17 19:52:11 -0300

    comentarios

---
+++ primeiro-projeto/src/Comentarios.java
@@ -0,0 +1,9 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Esta é uma linha de comentário que será ignorada pelo compilador
+
+        /* Este é um exemplo de comentário
+        de várias linhas em Java
+        que será ignorado pelo compilador */
+    }
+}
