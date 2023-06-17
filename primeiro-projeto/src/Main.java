e4cc6cb3529f74bec359810c04d1f25e31760da5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-17 11:03:15 -0300

    tirando a média do filme

--- primeiro-projeto/src/Main.java
+++ primeiro-projeto/src/Main.java
@@ -8,5 +8,8 @@ public class Main {
 
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
     }
 }
