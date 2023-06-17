b85b8667157eeb0c78389d01511fe785702d723b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-17 11:20:48 -0300

    adicionando comentários no código

--- primeiro-projeto/src/Main.java
+++ primeiro-projeto/src/Main.java
@@ -9,10 +9,19 @@ public class Main {
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
         double media = (9.8 + 6.3 + 8.0) / 3;
-        System.out.println(media);
-        
+        System.out.println("Média do filme: " + media);
+
         String sinopse;
         sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println("Sinopse do Filme: " + sinopse);
+
+        String descricao = """
+                    Filme: Top Gun: Maverick
+                    Ano de lançamento: 
+                """ + anoDeLancamento;
+
+        System.out.println(descricao);
     }
 }
