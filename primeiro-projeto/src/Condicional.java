05e5e254c8cb6c48099c3969e064e97fb2a55f45
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-17 19:25:11 -0300

    condicional no java

---
+++ primeiro-projeto/src/Condicional.java
@@ -0,0 +1,14 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        
+
+        if (anoDeLancamento >= 2022) {
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir");
+        }
+    }
+}
