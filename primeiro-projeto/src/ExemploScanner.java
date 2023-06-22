e4148f53a0389c2f01a12798955c77c7741c1918
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-22 18:54:39 -0300

    exemplo de Scanner

---
+++ primeiro-projeto/src/ExemploScanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ExemploScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        System.out.print("Digite o valor que pretende investir esse mês: ");
+        double valor = scanner.nextDouble();
+
+        System.out.println(nome + " que tem " + idade + " anos, irá investir R$ " + valor + " esse mês.");
+
+        scanner.close();
+    }
+}
