0d96dcbc9032b7c1a04975243157bdfcff94eb7f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-22 17:45:23 -0300

    formatações dos tipos de dados

--- primeiro-projeto/src/Leitura.java
+++ primeiro-projeto/src/Leitura.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 
 public class Leitura {
     public static void main(String[] args) {
+        //classe para ler valores na entrada de dados
         Scanner leitura = new Scanner(System.in);
 
         System.out.print("Digite seu filme favorito:");
-        String filme = leitura.nextLine();
+        String filme = leitura.nextLine(); //armazenando valor do tipo String
 
-        System.out.println(String.format("O filme digitado é %s", filme));
+        System.out.printf("Digite o ano de lançamento do %s:", filme);
+        int ano = leitura.nextInt();
+
+        System.out.printf("Digite a sua nota de 0 a 10 para o %s:", filme);
+        int nota = leitura.nextInt();
+
+        System.out.printf("\nFilme: %s", filme);
+        System.out.printf("\nAno: %d", ano);
+        System.out.printf("\nNota: %d", nota);
     }
 }
