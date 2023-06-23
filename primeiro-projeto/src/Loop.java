9e1d8b85091cdc5b778cab42c30af3a0dd5b6baf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-23 09:33:09 -0300

    programa que calcula média de notas de filme

---
+++ primeiro-projeto/src/Loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        int somaNotas = 0;
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite a nota: ");
+            int nota = leitura.nextInt();
+            somaNotas += nota;
+        }
+        //casting para que a média seja com ponto flutuante
+        double media = (double) somaNotas / 3;
+
+        System.out.printf("Avaliação do filme: %.1f", media);
+    }
+}
